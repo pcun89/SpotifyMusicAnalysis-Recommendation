83ee275e054e9dcba51301f041e352ff41251312
pcun89 <phillippcun89@gmail.com>
2026-01-16 15:21:46 -0600

    changes

--- spotify_client.py
+++ spotify_client.py
@@ -3,11 +3,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 
 def getSpotifyClient(clientId, clientSecret):
-    """
-    Creates and returns a Spotify API client.
-    """
     authManager = SpotifyClientCredentials(
         client_id=clientId,
         client_secret=clientSecret
     )
-    return spotipy.Spotify(auth_manager=authManager)
+
+    sp = spotipy.Spotify(auth_manager=authManager)
+    return sp
